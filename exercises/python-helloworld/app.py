9dc8cd1bb97f0d41c4ea61707de98147ac470bb2
Ifegwu <agbanyimdaniel29@gmail.com>
2022-10-04 15:50:32 +0200

    anding routes and logging features

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,42 @@
+from urllib import response
 from flask import Flask
+from flask import json
+import logging
+
 app = Flask(__name__)
+
+@app.route("/status")
+def healthCheck():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info("status request successful")
+    return response
+
+@app.route("/metrics")
+def metrics():
+  response = app.response_class(
+          response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+          status=200,
+          mimetype='application/json'
+  )
+  app.logger.info("metrics request successful")
+  return response
 
 @app.route("/")
 def hello():
+    # Logging a CUSTOM message
+    app.logger.info('Main request successfull')
     return "Hello World!"
-
+    
 if __name__ == "__main__":
+    # Stream logs to a file, and set the default log level to DEBUG
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    # logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S', filename='app.log')
+    # logging.basicConfig(
+    #     format='%(asctime)s %(levelname)-8s %(message)s',
+    #     level=logging.INFO,
+    #     datefmt='%Y-%m-%d %H:%M:%S')
     app.run(host='0.0.0.0')
